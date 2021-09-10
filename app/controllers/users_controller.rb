f6888b9725845a63f415c9c9a6edb76fb841a896
itagusu <ir060906@gmail.com>
2021-09-10 13:07:12 +0000

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[])
+    @user = current_user
+    @book = Book.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
 end
